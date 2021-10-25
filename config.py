af36802e7cdfbc88d4cff37da4c9304527173ff5
maybe-my <loading4512@gmail.com>
2021-10-25 16:25:28 +0300

    Update config.py
    test

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API = 'fc4fd1de889d4fd0ed1e807a30969ad4'
 MUSIC_URL = 'https://mp3zen.net/uploads/files/metan-bashnya-klinit.mp3'
 BOT_TOKEN = '2006504637:AAEDQU-x_lg58vYLy0R5UtCrMQV3nKqicC4'
-
+# Bot 
 # True - это если тест версия
 isTest = True
