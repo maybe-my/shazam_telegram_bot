c108763a70870f14ab73dd0ab7852959e7e1a447
maybe-my <84916243+maybe-my@users.noreply.github.com>
2022-09-30 21:56:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API = '1ebc455bee27e870f2de3b95427b79f9'
+API = ''
 MUSIC_URL = 'https://mp3zen.net/uploads/files/metan-bashnya-klinit.mp3'
-BOT_TOKEN = '2006504637:AAEDQU-x_lg58vYLy0R5UtCrMQV3nKqicC4'
+BOT_TOKEN = ''
 
 
 # True - это если тест версия
